52de6cb32b50f791b4b46b3835ec86cda3d7b256
felipeholbi <4758@holbertonstudents.com>
2022-12-19 21:27:33 -0500

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if not path.exists(argv[1]):
-        stderr.write(f'Missing {argv[1]}\n')
+        stderr.write("Missing {}\n".format(argv[1]))
         exit(1)
     else:
         exit(0)
