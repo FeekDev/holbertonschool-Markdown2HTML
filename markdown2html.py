51071f12589986e778127e270593d1d55a948d3f
felipeholbi <4758@holbertonstudents.com>
2022-12-19 21:18:52 -0500

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -9,7 +9,7 @@ from os import path
 
 if __name__ == "__main__":
     if len(argv) < 2:
-        stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if not path.exists(argv[1]):
         stderr.write(f'Missing {argv[1]}\n')
