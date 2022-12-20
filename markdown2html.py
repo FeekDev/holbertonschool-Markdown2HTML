7723b83f01ff0fe5afae074ca98f3b8353784fc8
felipeholbi <4758@holbertonstudents.com>
2022-12-19 21:14:55 -0500

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+'''
+This file read the comandline
+'''
 
 from sys import argv
 from sys import stderr
 from os import path
 
-if len(argv) < 2:
-    stderr.write('Usage: ./markdown2html.py README.md README.html\n')
-    exit(1)
-if not path.exists(argv[1]):
-    stderr.write(f'Missing {argv[1]}\n')
-    exit(1)
-else:
-    exit(0)
+if __name__ == "__main__":
+    if len(argv) < 2:
+        stderr.write('Usage: ./markdown2html.py README.md README.html\n')
+        exit(1)
+    if not path.exists(argv[1]):
+        stderr.write(f'Missing {argv[1]}\n')
+        exit(1)
+    else:
+        exit(0)
